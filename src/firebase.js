d94c0c6e7b25bee086a3d11159f2cc73e499faff
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-04 10:36:06 +0100

    setting up react and and firebase for tiktok dummy

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGvFS03bn-iGxZpAsJv82gI_gOHoS56xY",
+  authDomain: "tiktok-clone-7d7a2.firebaseapp.com",
+  projectId: "tiktok-clone-7d7a2",
+  storageBucket: "tiktok-clone-7d7a2.appspot.com",
+  messagingSenderId: "992808156503",
+  appId: "1:992808156503:web:42f833125e2df354cf9212",
+  measurementId: "G-6QBZCC9FKK",
+}
